f31758b84c39413422c0579ffd1b55536d233bfe
SqueakyCamp <68071888+SqueakyCamp@users.noreply.github.com>
2024-07-18 16:49:06 +0200

    removed the outdated scripts

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd scripts
+
+echo -e "Skip EtherCAT Master / EtherLAB installation and setup? (y/n)"
+read skip_ethercat
+
+if [[ "$skip_ethercat" == "n" || "$skip_ethercat" == "N" ]]; then
+	./ethercat_setup.sh
+fi
+
+
+echo -e "Skip EPICS-Base installation and setup? (y/n)"
+read skip_base
+
+if [[ "$skip_base" == "n" || "$skip_base" == "N" ]]; then
+	./epics-base_setup.sh
+fi
+
+echo -e "Skip EPICS-Support installation and setup? (y/n)"
+read skip_support
+
+if [[ "$skip_support" == "n" || "$skip_support" == "N" ]]; then
+	./epics-support_setup.sh
+fi
+
+echo -e "Skip EtherCAT for EPICS installation and setup? (y/n)"
+read skip_epics_ethercat
+
+if [[ "$skip_epics_ethercat" == "n" || "$skip_epics_ethercat" == "N" ]]; then
+	./epics-ethercat_setup.sh
+	cp update_ioc.sh ../
+fi
