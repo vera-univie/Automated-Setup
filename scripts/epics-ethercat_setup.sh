072a969a473f2a9ead0cb0a7d647a71d6f914cfe
SqueakyCamp <68071888+SqueakyCamp@users.noreply.github.com>
2024-07-23 11:17:52 +0200

    updated epics ethercat setup

--- scripts/epics-ethercat_setup.sh
+++ scripts/epics-ethercat_setup.sh
@@ -17,13 +17,13 @@
 
 
 
-#echo -e "\n-----------------------------------------------------------------------\n"
-#echo -e "\nPlease enter your EtherLAB directory: "
-#read etherlab_master
+echo -e "\n-----------------------------------------------------------------------\n"
+echo -e "\nPlease enter your EtherLAB directory: "
+read etherlab_master
 
 cd ../
 epics_dir=$(pwd)
-etherlab=/opt/bin/etherlab
+etherlab=/opt/etherlab/bin
 
 edit_paths () {
 	sed -i "/VERSION *=/c\VERSION=" $1
@@ -31,7 +31,9 @@ edit_paths () {
 	sed -i "/ETHERLABPREFIX *=/c\ETHERLABPREFIX=\$(ETHERLAB)/.." $1
 	sed -i "/ETHERCAT *=/c\ETHERCAT=$epics_dir/ethercat" $1
 	sed -i "/ECASYN *=/c\ECASYN=$epics_dir/ethercat" $1
-	sed -i "/ASYN *=/c\ASYN=$support/asyn" $1
+	sed -i "/ASYN *=/c\ASYN=$epics_dir/support/asyn" $1 # Look here
+	sed -i "/^SEQ *=/c\SEQ=$epics_dir/support/seq" $1
+	sed -i "/BUSY *=/c\BUSY=$epics_dir/support/busy" $1
 	sed -i "/SUPPORT *=/c\SUPPORT=$epics_dir/support" $1
 	sed -i "/EPICS_BASE *=/c\EPICS_BASE=$epics_dir/epics-base" $1
 }
@@ -40,14 +42,16 @@ edit_paths () {
 #git clone https://github.com/dls-controls/ethercat.git ethercat # change this once ready
 #cd ethercat
 #edit_paths ethercatApp/scannerSrc/Makefile
-#sed -i "/ETHERLABPREFIX *=/a\ETHERCAT_MASTER_ETHERLAB=$etherlab_master" ethercatApp/scannerSrc/Makefile
-#sed -i "/scanner_INCLUDES *+=/c\scanner_INCLUDES += -I\$(ETHERCAT_MASTER_ETHERLAB)/lib" ethercatApp/scannerSrc/Makefile
-#sed -i "/serialtool_INCLUDES *+=/c\serialtool_INCLUDES += -I\$(ETHERCAT_MASTER_ETHERLAB)/master" ethercatApp/scannerSrc/Makefile
-#sed -i "/get-slave-revisions_INCLUDES *+=/c\get-slave-revisions_INCLUDES += -I\$(ETHERCAT_MASTER_ETHERLAB)/master" ethercatApp/scannerSrc/Makefile
 
 git clone https://github.com/vera-univie/EPICS-EtherCAT.git ethercat
 cd ethercat
+edit_paths configure/RELEASE
 edit_paths ethercatApp/scannerSrc/Makefile
+
+sed -i "/ETHERLABPREFIX *=/a\ETHERCAT_MASTER_ETHERLAB=$etherlab_master" ethercatApp/scannerSrc/Makefile
+sed -i "/scanner_INCLUDES *+=/c\scanner_INCLUDES += -I\$(ETHERCAT_MASTER_ETHERLAB)/lib" ethercatApp/scannerSrc/Makefile
+sed -i "/serialtool_INCLUDES *+=/c\serialtool_INCLUDES += -I\$(ETHERCAT_MASTER_ETHERLAB)/master" ethercatApp/scannerSrc/Makefile
+sed -i "/get-slave-revisions_INCLUDES *+=/c\get-slave-revisions_INCLUDES += -I\$(ETHERCAT_MASTER_ETHERLAB)/master" ethercatApp/scannerSrc/Makefile
 
 apt install icu-devtools libicu-dev libxml2-dev -y
 
@@ -56,7 +60,7 @@ edit_paths etc/capture_wave/configure/RELEASE
 
 edit_paths iocs/scanTest/configure/RELEASE
 edit_paths iocs/veraBasic/configure/RELEASE
-edit_paths iocs/Backup_veraBasic/configure/RELEASE
+#edit_paths iocs/Backup_veraBasic/configure/RELEASE
 
 #edit_paths iocs/es3602-test/configure/RELEASE
 #edit_paths iocs/ni9144-rev4-w9239-slot1/configure/RELEASE
@@ -65,7 +69,12 @@ edit_paths iocs/Backup_veraBasic/configure/RELEASE
 #edit_paths iocs/iocTest/configure/RELEASE
 #edit_paths iocs/i12test/configure/RELEASE
 
+make
 
+cd iocs/scanTest
+make
+cd ../veraBasic
+make
 
 
 
